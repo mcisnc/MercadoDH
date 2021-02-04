199634affe2e2ede4465d9a0e2af2bccaccaa329
Max <mcisnerosc@uni.pe>
2021-02-04 18:56:16 -0500

    port + procfile || heroku

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ const path = require('path');
 
 const app = express();
 
-const port = 3000;
-
-app.listen(port, ()=>{console.log(`-> http://localhost:${port} <-`)});
+app.listen(process.env.PORT || 3000, ()=>{console.log(`-> http://localhost:${port} <-`)});
 
 app.use('/static', express.static(path.join(__dirname, './public')))
 
